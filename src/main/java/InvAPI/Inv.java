134a23772627afd34fc7602f4b14e33da827b76a
Daniel <Danidrums07@gmail.com>
2024-09-23 21:34:00 +0200

    -Added GetPlayerHead

--- src/main/java/InvAPI/Inv.java
+++ src/main/java/InvAPI/Inv.java
@@ -2,22 +2,31 @@ package InvAPI;
 
 import InvAPI.Code;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.block.ShulkerBox;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.SkullMeta;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.profile.PlayerProfile;
+import org.bukkit.profile.PlayerTextures;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.UUID;
 
 public class Inv {
     public Inventory inv;
@@ -103,13 +112,29 @@ public class Inv {
             listeners.put(item, code);
         }
     }
-
-    public void createPage(String inventoryDisplayName) {
-
-
-
+    private static final UUID RANDOM_UUID = UUID.fromString("92864445-51c5-4c3b-9039-517c9927d1b4");
+    private static PlayerProfile getProfile(String url) {
+        PlayerProfile profile = Bukkit.createPlayerProfile(RANDOM_UUID);
+        PlayerTextures textures = profile.getTextures();
+        URL urlObject;
+        try {
+            urlObject = new URL(url);
+        } catch (MalformedURLException exception) {
+            throw new RuntimeException("Invalid URL", exception);
+        }
+        textures.setSkin(urlObject);
+        profile.setTextures(textures);
+        return profile;
     }
-
+    public static ItemStack getPlayerHead(String name,  List<String> lore, String headURL) {
+        PlayerProfile profile = getProfile(headURL);
+        ItemStack head = new ItemStack(Material.PLAYER_HEAD);
+        SkullMeta meta = (SkullMeta) head.getItemMeta();
+        meta.setOwnerProfile(profile);
+        meta.setLore(lore);
+        head.setItemMeta(meta);
+        return head;
+    }
     public static List<String> createLore(String lore) {
         return Arrays.asList(lore.split("\n"));
     }
